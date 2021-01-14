8f939a4a2f5a47632b740048fb59e40d035028d3
Up Chen <up.chen@greenpeace.org>
2021-01-14 16:59:09 +0800

    Added xmlns:media to the RSS feed to ensure it fit the xml validation.

--- rss.php
+++ rss.php
@@ -27,6 +27,8 @@ do_action( 'rss_tag_pre', 'rss2' );
 	xmlns:atom="http://www.w3.org/2005/Atom"
 	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
 	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	xmlns:media="http://search.yahoo.com/mrss/"
+
 	<?php
 	/**
 	 * Fires at the end of the RSS root to add namespaces.
